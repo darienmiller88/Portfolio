ae101fe268cada66c9d0010ea3371e2623f12aca
darienmiller88 <darienmiller88@yahoo.com>
2021-12-15 18:50:21 -0500

    edited readme

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -11,23 +11,26 @@ export default function Home() {
     return (
         <div>
             <NavBar/>
-            <div className="name my-margin">
+            <div className="wrapper">
+                <div className="name my-margin">
                 Darien Miller.
+                </div>
+                <div className="slogan my-margin">
+                    Aspiring Software Engineer.
+                </div>             
+                <br/>
+                <p className="description my-margin">
+                    I'm a recent Computer Science graduate out of Brooklyn College <br/>
+                    seeking to further my education in Graduate school for Compputer Engineering.<br/>
+                    I specialize in fullstack development with a focus on backend development,<br/>
+                    and am currently seeking employment.
+                </p>
+                <br/>
+                <a className="github-link my-margin" href="https://github.com/darienmiller88">
+                    Check out my projects!
+                </a>
             </div>
-            <div className="slogan my-margin">
-                Aspiring Software Engineer.
-            </div>             
-            <br/>
-            <p className="description my-margin">
-                I'm a recent Computer Science graduate out of Brooklyn College <br/>
-                seeking to further my education in Graduate school for Compputer Engineering.<br/>
-                I specialize in fullstack development with a focus on backend development,<br/>
-                and am currently seeking employment.
-            </p>
-            <br/>
-            <a className="github-link my-margin" href="https://github.com/darienmiller88">
-                Check out my projects!
-            </a>
+            
         </div>
     )
 }
