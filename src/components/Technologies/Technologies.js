f6ed3127da75ddf1ffb72b368c7c9730da3f3387
darienmiller88 <darienmiller88@yahoo.com>
2022-01-13 23:38:57 -0500

    finally finished technologies

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -3,7 +3,6 @@ import "./Technologies.css"
 import SectionDivider from "../SectionDivider/SectionDivider"
 import TechnologyCard from "../TechnologyCard/TechnologyCard"
 import { FaReact, FaDatabase, FaFigma } from 'react-icons/fa'
-import { technologyConstants } from "../Constants/Constants"
 
 export default function Technologies() {
     return (
@@ -22,8 +21,8 @@ export default function Technologies() {
                     icon={<FaReact className='icon'/>}
                     header={"Front-End"}
                     description={
-                        "I have extensive experience in using Javascript frameworks to beautify and" +
-                        "modularize my projects. I have a strong preference for React.js, but I have" +
+                        "I have extensive experience in using Javascript frameworks to beautify and " +
+                        "modularize my projects. I have a strong preference for React.js, but I have " +
                         "also used Vue.js and Next.js."
                     }
                     link={"#project-section"}
@@ -32,8 +31,8 @@ export default function Technologies() {
                     icon={<FaDatabase className='icon'/>}
                     header={"Back-End"}
                     description={
-                        "Due to being primarily Back-end based, I can utilize multiple technologies" +
-                        "such as MongoDB, PostgreSQL, Node.js, MySQL, Heroku, Netlify, and Docker."
+                        "Due to being primarily Back-end based, I can utilize multiple technologies " +
+                        "such as MongoDB, PostgreSQL, Node.js, MySQL, Heroku, Netlify, and Docker. "
                     }
                     link={"#project-section"}
                 />
@@ -45,57 +44,7 @@ export default function Technologies() {
                     }
                     link={"https://www.figma.com/"}
                 />
-                {/* <div className="technology">
-                    <div className="icon-div">
-                        <FaReact className="icon"/>
-                    </div>  
-                    <div className="technology-header">
-                        <h3>Front-End</h3>
-                    </div>     
-                    <div className="technology-description">
-                        <p>
-                        I have extensive experience in using Javascript frameworks to beautify 
-                        and modularize my projects. I have a strong preference for React.js, but I have 
-                        also used Vue.js and Next.js.
-                        </p>
-                    </div>
-                    <div className="button-section">
-                        <a href='#project-section'>Read more</a>
-                    </div>
-                </div>
-                <div className="technology">
-                    <div className="icon-div">
-                        <FaDatabase className="icon"/>
-                    </div>
-                    <div className="technology-header">
-                        <h3>Back-End</h3>
-                    </div>    
-                    <div className="technology-description">
-                        <p>
-                            Due to being primarily Back-end based, I can utilize multiple technologies
-                            such as MongoDB, PostgreSQL, Node.js, MySQL, Heroku, Netlify, and Docker.
-                        </p>
-                    </div>
-                    <div className="button-section">
-                        <a href="#project-section">Read more</a>
-                    </div>
-                </div>
-                <div className="technology">
-                   <div className="icon-div">
-                        <FaFigma className="icon"/>
-                    </div> 
-                    <div className="technology-header">
-                        <h3>UI/UX</h3>
-                    </div> 
-                    <div className="technology-description">
-                        <p>
-                            I have achieved success in the UI/UX field using schematic tools such as Figma.
-                        </p>
-                    </div>
-                    <div className="button-section">
-                        <a href="https://www.figma.com/">Read more</a>
-                    </div>
-                </div> */}
+                
             </div> 
             <SectionDivider/>
         </div>
--- src/components/TechnologyCard/TechnologyCard.js
+++ src/components/TechnologyCard/TechnologyCard.js
@@ -1,27 +1,22 @@
 import React from 'react'
 import "./TechnologyCard.css"
-import { FaReact, FaDatabase, FaFigma } from 'react-icons/fa'
-import { technologyConstants } from "../Constants/Constants"
-
 
 export default function TechnologyCard(props) {
     return (
         <div className="technology-card">
             <div className="icon-div">
-                {
-                   props.icon
-                }
+                { props.icon }
             </div>
             <div className="technology-header">
-                <h3>{props.header}</h3>
+                <h3>{ props.header }</h3>
             </div>    
             <div className="technology-description">
                 <p>
-                    {props.description}
+                    { props.description }
                 </p>
             </div>
             <div className="button-section">
-                <a href={props.link}>Read more</a>
+                <a href={ props.link }>Read more</a>
             </div>
         </div>
     )
